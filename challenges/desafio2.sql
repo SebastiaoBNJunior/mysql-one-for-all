29439e3b5aa951fe8feee2d36f75d2ce84d850d1
sebastiaobnj <junin_sbn@hotmail.com>
2023-11-10 02:54:28 -0300

    testando req2

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,4 +1,4 @@
-SELECT
-    (SELECT COUNT(*) FROM SpotifyClone.cancao) AS cancoes,
-    (SELECT COUNT(*) FROM SpotifyClone.artista) AS artistas,
-    (SELECT COUNT(*) FROM SpotifyClone.album) AS albuns;
+SELECT COUNT(DISTINCT m.id) AS cancoes, COUNT(DISTINCT a.id) AS artistas, COUNT(DISTINCT al.id) AS albuns 
+    FROM musicas m 
+    LEFT JOIN artistas a ON m.artista_id = a.id
+    LEFT JOIN albuns al ON al.id = m.album_id;
